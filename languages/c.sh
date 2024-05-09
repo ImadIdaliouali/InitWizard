09c5e8700ab97ea0b018c3e4067a6144395071f5
AbderrahimeEl <elmoutaouakil12345@gmail.com>
2024-05-10 00:18:00 +0100

    Added JavaScript shell script and updated C/C++ .

--- languages/c.sh
+++ languages/c.sh
@@ -3,19 +3,55 @@
 project_name="$1"
 git="$2"
 
-mkdir "$project_name"
+mkdir $project_name
+cd $project_name
 
-cd "$project_name"
+# Create include, src, docs, and bin directories
+mkdir include src docs bin
 
-echo "#include <stdio.h>
+# Create .h file inside include directory
+echo "#ifndef ${project_name^^}_H
+#define ${project_name^^}_H
 
-int main()
-{
-    printf(\"Hello World!\n\");
+#include <stdio.h>
+
+#endif" > include/$project_name.h
+
+# Create .c files inside src directory
+echo "#include \"../include/$project_name.h\"
+#include <stdio.h>
+
+int main() {
+    printf(\"Hello, world!\\n\");
     return 0;
-}" > main.c
+}" > src/main.c
 
+# Create Makefile
+echo "CC = gcc
+CFLAGS = -Wall -Wextra -std=c99 -Iinclude
+SRC_DIR = src
+OBJ_DIR = obj
+BIN_DIR = bin
+SRC = \$(wildcard \$(SRC_DIR)/*.c)
+OBJ = \$(SRC:\$(SRC_DIR)/%.c=\$(OBJ_DIR)/%.o)
+EXECUTABLE = \$(BIN_DIR)/$project_name
+
+all: \$(EXECUTABLE)
+
+\$(EXECUTABLE): \$(OBJ)
+\t\$(CC) \$(CFLAGS) \$^ -o \$@
+
+\$(OBJ_DIR)/%.o: \$(SRC_DIR)/%.c
+\t\$(CC) \$(CFLAGS) -c \$< -o \$@
+
+clean:
+\trm -rf \$(OBJ_DIR)/* \$(BIN_DIR)/*" > Makefile
+
+# Create README.md
 echo "# $project_name" > README.md
+
+# Create docs folder
+mkdir docs
 
 if [ "$git" = "yes" ]; then
     git init
--- languages/cpp.sh
+++ languages/cpp.sh
@@ -4,20 +4,54 @@ project_name="$1"
 git="$2"
 
 mkdir $project_name
-
 cd $project_name
 
-echo "#include <iostream>
+# Create include, src, docs, and bin directories
+mkdir include src docs bin
 
+# Create .hpp file inside include directory
+echo "#ifndef ${project_name^^}_HPP
+#define ${project_name^^}_HPP
+
+#include <iostream>
+
+#endif" > include/$project_name.hpp
+
+# Create .cpp files inside src directory
+echo "#include \"../include/$project_name.hpp\"
+#include<iostream>
 using namespace std;
-
-int main()
-{
-    cout << \"Hello World!\" << endl;
+int main() {
+    cout << \"Hello World!\" << std;
     return 0;
-}" > main.cpp
+}" > src/main.cpp
 
+# Create Makefile
+echo "CC = g++
+CFLAGS = -std=c++11 -Iinclude
+SRC_DIR = src
+OBJ_DIR = obj
+BIN_DIR = bin
+SRC = \$(wildcard \$(SRC_DIR)/*.cpp)
+OBJ = \$(SRC:\$(SRC_DIR)/%.cpp=\$(OBJ_DIR)/%.o)
+EXECUTABLE = \$(BIN_DIR)/$project_name
+
+all: \$(EXECUTABLE)
+
+\$(EXECUTABLE): \$(OBJ)
+\t\$(CC) \$(CFLAGS) \$^ -o \$@
+
+\$(OBJ_DIR)/%.o: \$(SRC_DIR)/%.cpp
+\t\$(CC) \$(CFLAGS) -c \$< -o \$@
+
+clean:
+\trm -rf \$(OBJ_DIR)/* \$(BIN_DIR)/*" > Makefile
+
+# Create README.md
 echo "# $project_name" > README.md
+
+# Create docs folder
+mkdir docs
 
 if [ "$git" = "yes" ]; then
     git init
